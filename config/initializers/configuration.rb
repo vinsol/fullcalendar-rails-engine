2835a458650d4e28fca9fe5d89aea268e40387b2
aditya-kapoor <aditya.kapoor@vinsol.com>
2014-01-02 21:05:39 +0530

    moved some config options to the configuration.rb

--- config/initializers/configuration.rb
+++ config/initializers/configuration.rb
@@ -1,15 +1,17 @@
-config = File.exists?('config/fullcalendar.yml') ? YAML.load_file("config/fullcalendar.yml") : {}
+config = File.exists?('config/fullcalendar.yml') ? YAML.load_file("config/fullcalendar.yml") || {} : {}
 FullcalendarEngine::Configuration = {
-  'editable' => true,
-  'header' => {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay'
-          },
+  'editable'    => true,
+  'header'      => {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'month,agendaWeek,agendaDay'
+  },
   'defaultView' => 'agendaWeek',
-  'height' => 500,
+  'height'      => 500,
   'slotMinutes' => 15,
   'dragOpacity' => 0.5,
-  'selectable' => true
+  'selectable'  => true,
+  'timeFormat'  => "h:mm t{ - h:mm t}"
 }
 FullcalendarEngine::Configuration.merge!(config)
+FullcalendarEngine::Configuration['events'] = "#{FullcalendarEngine::Configuration['mount_path']}/events/get_events"
