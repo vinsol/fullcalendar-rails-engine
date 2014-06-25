14468afef0b6ad16bb92a95ab6322cfbf6707ffc
Aditya Kapoor <aditya.kapoor@vinsol.com>
2014-06-25 23:19:01 +0530

    Fix Query for the fetching of the events on calendar

--- app/controllers/fullcalendar_engine/events_controller.rb
+++ app/controllers/fullcalendar_engine/events_controller.rb
@@ -23,10 +23,15 @@ module FullcalendarEngine
     end
 
     def get_events
-      @events = Event.where('starttime  >= :start_time and 
-                            endtime     <= :end_time',
-                            start_time: Time.at(params['start'].to_i).to_formatted_s(:db),
-                            end_time:   Time.at(params['end'].to_i).to_formatted_s(:db))
+      start_time = Time.at(params[:start].to_i).to_formatted_s(:db)
+      end_time   = Time.at(params[:end].to_i).to_formatted_s(:db)
+
+      @events = Event.where('
+                  (starttime >= :start_time and endtime <= :end_time) or
+                  (starttime >= :start_time and endtime > :end_time and starttime <= :end_time) or
+                  (starttime <= :start_time and endtime >= :start_time and endtime <= :end_time) or
+                  (starttime <= :start_time and endtime > :end_time)',
+                  start_time: start_time, end_time: end_time)
       events = []
       @events.each do |event|
         events << { id: event.id,
